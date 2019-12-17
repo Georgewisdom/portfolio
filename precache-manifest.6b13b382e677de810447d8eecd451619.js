0d4dea6b7fb1f307fac7a69ffa84915c1b90a820
Georgewisdom <coppycatter100@gmail.com>
2019-12-17 13:36:53 +0100

    Updates

---
+++ precache-manifest.6b13b382e677de810447d8eecd451619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e980e4d3a5710d85597c0a47963b4871",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5b14b52be6f8f0cd9f1e",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5de506047cad30a3bd2b",
+    "url": "/portfolio/static/js/2.fe4e2b0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.fe4e2b0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b14b52be6f8f0cd9f1e",
+    "url": "/portfolio/static/js/main.a8765216.chunk.js"
+  },
+  {
+    "revision": "424076916feab3dca05b",
+    "url": "/portfolio/static/js/runtime-main.b3729e50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
